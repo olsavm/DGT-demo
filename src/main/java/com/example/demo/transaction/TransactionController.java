7a9ac4f1d9085840cd686e69f322561cd2abb755
Olsav Michal <michal.olsav@extern.a1.at>
2024-01-14 13:40:01 +0100

    fix: add exception to prevent bad type inputs

--- src/main/java/com/example/demo/transaction/TransactionController.java
+++ src/main/java/com/example/demo/transaction/TransactionController.java
@@ -3,7 +3,9 @@ package com.example.demo.transaction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController()
 @RequestMapping("transactions")
@@ -26,7 +28,7 @@ public class TransactionController {
     }
 
     @GetMapping("/{id}/fee")
-    public Double getTransactionFee(@PathVariable String id) throws Exception {
+    public Double getTransactionFee(@PathVariable String id) {
         return transactionService.getTransactionFee(id);
     }
 }
--- src/main/java/com/example/demo/transaction/TransactionService.java
+++ src/main/java/com/example/demo/transaction/TransactionService.java
@@ -2,8 +2,11 @@ package com.example.demo.transaction;
 
 import com.example.demo.feeCalculator.FeeCalculator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -16,6 +19,9 @@ public class TransactionService {
     }
 
     public Transaction createTransaction(Transaction transaction) {
+        if (!transaction.getType().equals("international") && !transaction.getType().equals("national")) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Invalid Transaction type");
+        }
         return transactionRepository.save(transaction);
     }
 
@@ -23,14 +29,14 @@ public class TransactionService {
         return transactionRepository.findById(id);
     }
 
-    public Double getTransactionFee(String id) throws Exception {
+    public Double getTransactionFee(String id) {
         Optional<Transaction> transaction = transactionRepository.findById(id);
         FeeCalculator calculator = new FeeCalculator();
 
         if (transaction.isPresent()) {
             return calculator.getFee(transaction.get().getType());
         } else {
-            throw new Exception("Transaction not found");
+            return null;
         }
 
     }
